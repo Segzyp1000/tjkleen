59dd5e52585292d00ebd99bb483be932d2d0a573
Segun Olowoyeye <segunolowoyeye@gmail.com>
2026-02-15 16:58:49 +0100

    changed color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,7 +68,7 @@ const Navbar = () => {
                 <LogoIcon className="w-full h-full drop-shadow-sm" />
               </div>
               <div className="ml-3 flex flex-col">
-                <span className="text-2xl font-black tracking-tighter text-blue-500 leading-none">
+                <span className="text-2xl font-black tracking-tighter text-blue-400 leading-none">
                   TjayKleen
                 </span>
                 <span className="text-[9px] text-gray-600 font-bold uppercase tracking-[0.4em] mt-1.5 opacity-80">
